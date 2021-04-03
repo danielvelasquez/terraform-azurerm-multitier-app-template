620667237b89086b5eabe47691d9fc19eebc9c9a
daniel <abdavl@almbrand.dk>
2021-04-03 17:34:58 +0200

    Add descriptions to vars and outputs

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,29 @@
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.aks-leo-cluster.kube_config_raw
+  value       = azurerm_kubernetes_cluster.aks-leo-cluster.kube_config_raw
+  description = "Configuration of the provisioned kubernetes cluster"
 }
 
 output "cluster_ca_certificate" {
-  value = azurerm_kubernetes_cluster.aks-leo-cluster.kube_config.0.cluster_ca_certificate
+  value       = azurerm_kubernetes_cluster.aks-leo-cluster.kube_config.0.cluster_ca_certificate
+  description = "ca certificate for kubernetes cluster"
 }
 
 output "client_certificate" {
-  value = azurerm_kubernetes_cluster.aks-leo-cluster.kube_config.0.client_certificate
+  value       = azurerm_kubernetes_cluster.aks-leo-cluster.kube_config.0.client_certificate
+  description = "Cluster client certificate"
 }
 
 output "client_key" {
-  value = azurerm_kubernetes_cluster.aks-leo-cluster.kube_config.0.client_key
+  value       = azurerm_kubernetes_cluster.aks-leo-cluster.kube_config.0.client_key
+  description = "Cluster client key"
 }
 
 output "host" {
-  value = azurerm_kubernetes_cluster.aks-leo-cluster.kube_config.0.host
+  value       = azurerm_kubernetes_cluster.aks-leo-cluster.kube_config.0.host
+  description = "TCluster host"
 }
 
 output "resource_group_name" {
-  value = azurerm_resource_group.aks-rg.name
+  value       = azurerm_resource_group.aks-rg.name
+  description = "Resource group for cluster and associated components"
 }
--- variables.tf
+++ variables.tf
@@ -1,44 +1,54 @@
 variable "subscription_id" {
+  description = "Azure subscription identifier"
 }
 
 variable "backend_client_id" {
+  description = "Service principal used to provision resources"
 }
 
 variable "backend_client_secret" {
+  description = "Service Principal Key for authentication"
 }
 
 variable "tenant_id" {
+  description = "Tenant identifier on azure"
 }
 
 variable "location" {
- type = string
-  default = "westeurope"
+ type         = string
+  default     = "westeurope"
+  description = "Location for provisioned resources"
 }
 
 variable "kubernetes_version" {
   default = "1.19.6"
+  description = "TKubernetes version used for aks cluster"
 }
 
 variable "backend_ssh_key" {  
+  description = "ssh key to authenticate to backend"
 }
 
 variable resource_group_name {
-
+  description = "Name of the resource group to be created for each consuming project"
 }
 
 variable "prefix" {
-  default = "leo"
+  default     = "leo"
+  description = "Prefix for resource naming conventions"
 }
 
 variable "display_name_prefix" {
-  default = "leo-pharma"
+  default     = "leo-pharma"
+  description = "Display name prefix"
 }
 
 variable "environment" {
+  description = "Environment/stage used by consuming project to provision"
 }
 
 #################### from k8s
 
 variable "db_name" {
-
+  description = "Name of database resource for the consuming project"
 }
