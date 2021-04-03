def5b083dbe5cf2c86363afa03be58e4dd09ecbe
daniel <abdavl@almbrand.dk>
2021-04-03 23:59:18 +0200

    add insights dependency on rg

--- insights.tf
+++ insights.tf
@@ -1,6 +1,6 @@
 resource "azurerm_application_insights" "app-insights" {
   name                = "tf-test-appinsights"
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.aks-rg.name
   application_type    = "web"
 }
