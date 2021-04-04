1bcfd015cea6eb207a4906528f7fce2d0b8743d4
daniel <abdavl@almbrand.dk>
2021-04-04 11:25:11 +0200

    ensure dependency between resource group and virtual network

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "azurerm_resource_group" "aks-rg" {
 resource "azurerm_virtual_network" "leo-vn" {
   name                = "${var.prefix}-network"
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.aks-rg.name
   address_space       = ["10.1.0.0/16"]
 }
 
